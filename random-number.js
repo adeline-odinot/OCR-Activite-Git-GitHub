5c9238fd4751948d0e940d4798f7a4eeee38d3a0
adeline-odinot <adeline.odinot1997@gmail.com>
2018-10-11 18:19:19 +0200

    Ajout de commentaires dans le fichier pour une meilleure compréhension de la fonction random.

--- random-number.js
+++ random-number.js
@@ -1,19 +1,21 @@
 function random() {
+    
+    // Initialisation des différentes var utile pour le bon fonctionnement de la fonction random.
     var randomNumber = Math.round(Math.random() * 300 ) + 1;
-
     var txtRandomNumber = document.getElementById("random-number");
-    
-    txtRandomNumber.textContent = "Numéro aléatoire : " + randomNumber;
-    
     var parity = document.getElementById("parity");
     
+    // Un affichage du texte comportant le nombre aléatoire se met en place
+    txtRandomNumber.textContent = "Numéro aléatoire : " + randomNumber;
+    
+    // Texte affichant si un chiffre est pair ou impair se mettant en place selon sa condition.
     if (randomNumber % 2 === 0) {
         parity.textContent = randomNumber + " est un chiffre pair";
     }
-    
     else {
         parity.textContent = randomNumber + " est un chiffre impair";
     }
 }
 
+// Appel de la fonction random
 random();
